1c855663c28f1011bdde1456bb36ab078ec2e1d3
azilvitis <azilviti@calpoly.edu>
2017-05-07 16:32:18 -0700

    Update linked_list_tests.py

--- linked_list_tests.py
+++ linked_list_tests.py
@@ -1,91 +1,80 @@
-# An AnyList is a:
-# - None is Empty
-# - Pair(first,rest)
+import unittest
+from linked_list import *
 
-class Pair:
-   def __init__(self,first,rest):
-      self.first = first # first is any value
-      self.rest = rest # rest is an AnyList
-
-   def __repr__(self):
-      return 'Pair({}, {})'.format(self.first,self.rest)
-
-   def __eq__(self,other):
-      return type(other) == Pair and self.first == other.first and self.rest == other.rest
-
-# empty_list : 
-# Returns an empty list.
-def empty_list():
-   return None
-
-# add : LinkedList int val count -> LinkedList
-# Returns a list containing a value at the indicated location.
-def add(lst,index,value,count=0):
-   if index < 0:
-      raise IndexError()
-   if count == index:
-      if lst == None:
-         return Pair(value, None)
-      else:
-         return Pair(value, Pair(lst.first,lst.rest))
-   elif count != index and lst == None:
-      raise IndexError()
-   else:
-      return Pair(lst.first, add(lst.rest, index, value, count+1))
-
-# length : LinkedList -> int
-# Returns length of list.
-def length(lst):
-   if lst == None:
-      return 0
-   else:
-      return 1 + length(lst.rest)
-
-# get : LinkedList int -> val
-# Returns the value at specified index.
-def get(lst, index):
-   if lst == None:
-      raise IndexError()
-   if index < 0:
-      raise IndexError()
-   if index == 0:
-      return lst.first
-   else:
-      return get(lst.rest, index-1)
-
-# set : LinkedList int value -> LinkedList
-# Returns a list with a value replacing the previous value at a specified index.
-def set(lst,index,value,acc=0):
-   if index >= length(lst)+acc or index < 0:
-      raise IndexError()
-   elif acc != index:
-      return Pair(lst.first,set(lst.rest,index,value,acc+1))
-   else:
-      return Pair(value, lst.rest)
-
-# remove : LinkedList int int -> LinkedList
-# Returns a list with the value removed at a specified index.
-def remove(lst,index,count=0):
-   if index < 0 or lst == None or index < count:
-      raise IndexError()
-   elif index == count:
-      return (lst.first, lst.rest)
-   else:
-      return (remove(lst.rest, index, count + 1)[0],Pair(lst.first,remove(lst.rest, index, count+1)[1]))
-
-
-# foreach : LinkedList func -> None
-# Applies function to list iteratively.
-def foreach(lst,func):
-   if lst:
-      func(lst.first)
-      foreach(lst.rest,func)
-   return None
-
-# str_list : LinkedList -> None
-# Prints values of list as a str representation.
-def str_list(lst):
-   if lst:
-      print(str(lst.first))
-      str_list(lst.rest)
-   return None
+class TestList(unittest.TestCase):
+    # Note that this test doesn't assert anything! It just verifies your
+    #  class and function definitions.
+   def test_interface(self):
+      temp_list = empty_list()
+      temp_list = add(temp_list, 0, "Hello!")
+      length(temp_list)
+      get(temp_list, 0)
+      temp_list = set(temp_list, 0, "Bye!")
+      remove(temp_list, 0)
+   def test_repr(self):
+      t = Pair("Food", Pair(2, None))
+      self.assertEqual(repr(t),'Pair(Food, Pair(2, None))')
+   def test_mt(self):
+      self.assertEqual(empty_list(),None)
+   def test_add(self):
+      temp_list = Pair("Food", Pair(2, None))
+      self.assertEqual(add(temp_list,1,"Hi"), Pair("Food", Pair("Hi", Pair(2,None))))
+   def test_add_2(self):
+      temp_list = Pair(1, Pair(3, Pair("Here",None)))
+      self.assertEqual(add(temp_list,0,2), Pair(2, Pair(1, Pair(3, Pair("Here",None)))))
+   def test_add_none(self):
+      temp_list = None
+      self.assertEqual(add(temp_list,0,3),Pair(3, None))
+   def test_length(self):
+      temp_list = Pair("Food", Pair(2, None))
+      self.assertEqual(length(temp_list), 2)
+   def test_add_err(self):
+      with self.assertRaises(IndexError):
+         add(Pair("This", Pair("will", Pair("fail", None))), -1, "me")
+   def test_add_err_2(self):
+      with self.assertRaises(IndexError):
+         add(None, -1, 2)
+   def test_add_err_3(self):
+      with self.assertRaises(IndexError):
+         add(None, 3, 4)
+   def test_add_err_4(self):
+      with self.assertRaises(IndexError):
+         add(Pair("Foof", Pair("LOOF",None)),3,5,7)
+   def test_add_none_again(self):
+      self.assertEqual(add(None,0,3), Pair(3, None))
+   def test_get(self):
+      temp_list = Pair("Food", Pair(2, None))
+      self.assertEqual(get(temp_list, 1),2)
+   def test_get_err(self):
+      with self.assertRaises(IndexError):
+      get(Pair("How",Pair("to",Pair("unnittest",Pair("errors",None)))), 7)
+   def test_get_none(self):
+      with self.assertRaises(IndexError):
+      get(Pair("here", Pair("lies",None)), -1)
+   def test_set(self):
+      temp_list = Pair("Food", Pair(2, None))
+      self.assertEqual(set(temp_list, 1, "Bye!"),Pair("Food",Pair("Bye!",None)))
+   def test_set_err(self):
+      with self.assertRaises(IndexError):
+         set(Pair("Here",Pair("is",None)),-1,3)
+   def test_rem_err(self):
+      with self.assertRaises(IndexError):
+         remove(Pair("Here",Pair("is",None)),-1)
+   def test_set_0(self):
+      temp_list = Pair(1,None)
+      self.assertEqual(set(temp_list,0,2), Pair(2,None))
+   def test_remove(self):
+      temp_list = Pair("Food", Pair(2, None))
+      self.assertEqual(remove(temp_list,1),(2,Pair("Food",None)))
+   def test_remove_1(self):
+      temp_list = Pair(1, Pair("Hey", Pair(4, None)))
+      self.assertEqual(remove(temp_list,0),(1,Pair("Hey", Pair(4,None))))
+   def test_remove_2(self):
+      temp_list = Pair(1, Pair("Hey", Pair(4, None)))
+      with self.assertRaises(IndexError):
+      remove(temp_list,-1)
+   def test_remove_3(self):
+      temp_list = Pair(1, Pair("Bye", Pair(5, None)))
+      self.assertEqual(remove(temp_list,2),(5,Pair(1,Pair("Bye",None))))
+if __name__ == '__main__':
+    unittest.main()
